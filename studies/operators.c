55c53f7fce713b2a7cf0dfdcd7c23cc9e324c72f
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-07-18 23:25:03 -0300

    operators

---
+++ studies/operators.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main (void){
+
+    int a = 10, b = 15;
+
+    int sum =  a + b;
+
+    printf("%d + %d = %d\n",a,b, sum);
+
+
+    int menos = a - b;
+
+    printf("%d - %d = %d\n",a,b,menos);
+
+    int mult = a * b;
+
+    printf("%d * %d = %d\n",a,b, mult);
+
+    int div = a / b;
+
+    printf("%f\n", div);
+
+    int resto = a % b;
+
+    printf("%d\n", resto);
+
+    a++;
+
+    printf("incremento: %d\n", a);
+
+    b--;
+
+    printf("decremento: %d\n", b);
+
+}

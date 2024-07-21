390af4dc17d66ecb4b7afbad3407441986e271ba
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2024-07-21 15:23:19 -0300

    type in c

---
+++ studies/type.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+  // Declara uma variável "a" do tipo inteiro com tamanho reduzido
+  short a = 32767;
+
+  // Declara uma variável "b" do tipo inteiro com tamanho aumentado
+  long b = 123456789;
+
+  // Declara uma variável "c" do tipo inteiro com sinal
+  signed c = -123;
+
+  // Declara uma variável "d" do tipo inteiro sem sinal
+  unsigned d = 123;
+
+  // Declara uma variável "e" do tipo ponto flutuante com precisão simples
+  float e = 3.14;
+
+  // Declara uma variável "f" do tipo ponto flutuante com precisão dupla
+  double f = 3.14159265358979323846;
+
+  return 0;
+}
